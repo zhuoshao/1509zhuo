7204aec3244e824888c72095f818a6b54b4dc405
zhuo@hotmail.com <tarena2@liujie.local>
2015-12-29 19:44:57 +0800

    进行简单的终端git创建 文本仓库和xcode 来检验

---
+++ 撒旦撒/撒旦撒/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  撒旦撒
+//
+//  Created by zhuo  on 15/12/29.
+//  Copyright © 2015年 zhuo . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 撒旦撒/撒旦撒/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  撒旦撒
+//
+//  Created by zhuo  on 15/12/29.
+//  Copyright © 2015年 zhuo . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+     NSLog(@"git 的使用 xcode 和 终端 和可视化软件  git就是一个 应用 苹果自带有这个 可以用git 通过cd 设置创建一个文件的目录  mkdir 来创建一个文件夹  然后   使用 git init  可以创建初始化化本地仓库  然后会产生影藏的额三个文件  其中。git  然后使用配置 git config user。name zzz 配置用户的名字   然后再执行git config  user。email zzz @hotmail。com 配置邮箱。。  就可以  git add  加入到缓存区 ，  可以使用git status  可以查看当前状态 ，，， 然后使用图git commoit 把内容添加到可以看出由红色变成 绿色  仓库中repositoy  可以再继续使用git status  查看，你可以 git refiog 分支应引用的记录  通过git reset --hard 8146023 那个你想区的版本 然后直接可以进行修改 操作后才上床就好");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
